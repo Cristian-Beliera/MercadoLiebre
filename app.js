edab92270fe0efe472ad9d008cdb8a8ceef552b7
Comision19 <cristianbeliera@gmail.com>
2022-12-12 21:07:37 -0300

    Creacion Mercado Liebre

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 3000;
+
+app.use(express.static("public"))
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+})
+
+app.listen(PORT, () => console.log(`Server listen in port ${PORT}
+http://localhost:${PORT}`))
+
+
+/* const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+   return res.sendFile(path.join(__dirname, "/views/home.html"));
+})
+
+app.listen(PORT, () => console.log(`
+Server listen in port ${PORT}
+http://localhost:${PORT}
+`))
+ */
